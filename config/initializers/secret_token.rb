44f6c9e479d203d54111aefbd56663e557c90571
Adit Biswas <aditbiswas1@gmail.com>
2013-06-04 22:45:41 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eforea::Application.config.secret_token = '7462aeb4432169db7f7ca4139375ecc7192f0165658cd4117585a16350600556e294ce0ae2c031c2265917d847aed7a34a15ea8ff1f4e729c0cdd2319490394a'
